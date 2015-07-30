02ff52b3875a17047d41212c8abfa65878707dad
Jan Oelze <janoelzelebt@googlemail.com>
2015-07-30 13:28:31 +0200

    Update main.py
    Run app on 0.0.0.0:80

--- main.py
+++ main.py
@@ -46,4 +46,8 @@ def generate():
         return jsonify({'success': False})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=80,
+        debug=debug
+    )
